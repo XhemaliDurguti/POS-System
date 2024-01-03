ff448cdb4c90a849b09c4e7b34df5fce79c70deb
XhemaliDurguti <xdurguti7@gmail.com>
2024-01-03 23:31:03 +0100

    Update Taxid.php
    Zbritja ndryshimi si dhe design i ndryshimit

--- admin/taxids.php
+++ admin/taxids.php
@@ -3,6 +3,7 @@ include_once '../include/db.php';
 session_start();
 include_once 'includes/header.php';
 
+//Regjistrimi i Zbritjes
 if (isset($_POST['btnsave'])) {
     $zperqindje = $_POST['txtzbritjaP'];
     $zmoney = $_POST['txtzbritjaM'];
@@ -20,6 +21,31 @@ if (isset($_POST['btnsave'])) {
 
         if ($insert->execute()) {
             $_SESSION['status'] = "Zbritja u Regjistrua me sukses!!";
+            $_SESSION['status_code'] = "success";
+        } else {
+            $_SESSION['status'] = "Diqka shkoj gabim provoni perseri!!";
+            $_SESSION['status_code'] = "error";
+        }
+    }
+}
+
+if(isset($_POST['btnupdate'])) {
+    $zid = $_POST['zid'];
+    $zperqindje = $_POST['txtzbritjaP'];
+    $zmoney = $_POST['txtzbritjaM'];
+    $zbritja = $_POST['txtzbritja'];
+
+    if(empty($zperqindje)) {
+        $_SESSION['status'] = "Nuk i keni plotesuar hapisrat e kerkuara!";
+        $_SESSION['status_code'] = "warning";
+    }else {
+        $update = $pdo->prepare("update tbl_zbritja set zbritjaPerqind=:zp,zbritjaPare=:zm,VleraZbritjes=:vz where zid =".$zid);
+        $update->bindParam(':zp',$zperqindje);
+        $update->bindParam(':zm',$zmoney);
+        $update->bindParam(':vz',$zbritja);
+
+        if ($update->execute()) {
+            $_SESSION['status'] = "Zbritja u ndryshua me sukses!!";
             $_SESSION['status_code'] = "success";
         } else {
             $_SESSION['status'] = "Diqka shkoj gabim provoni perseri!!";
@@ -55,7 +81,40 @@ if (isset($_POST['btnsave'])) {
                 <div class="card-body">
                     <form action="" method="post">
                         <div class="row">
-                            <div class="col-md-4">
+                            <?php 
+                                if(isset($_POST['btnedit'])) {
+                                    $select = $pdo->prepare("select * from tbl_zbritja where zid = ".$_POST['btnedit']);
+                                    $select->execute();
+                                    if ($select) {
+                                        $row = $select->fetch(PDO::FETCH_OBJ);
+                                    echo '<div class="col-md-4">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label>Zbritja(%)</label>
+                                        <input type="hidden" class="form-control" placeholder="zid" name="zid" value="'.$row->zid.'"/>
+                                        <input type="text" class="form-control" placeholder="Vlera Zbritjes ne %" value="'.$row->zbritjaPerqind.'"name="txtzbritjaP">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Zbritja ne Pare</label>
+                                        <input type="hidden" class="form-control" placeholder="id" name="catid" />
+                                        <input type="text" class="form-control" placeholder="Vlera Zbritjes ne Kesh" name="txtzbritjaM" value="'.$row->zbritjaPare.'">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Zbritja</label>
+                                        <input type="hidden" class="form-control" placeholder="id" name="catid" />
+                                        <input type="text" class="form-control" placeholder="Zbritja Finale" name="txtzbritja" value="'.$row->VleraZbritjes.'">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-info" name="btnupdate">Ndrysho Zbritjen</button>
+                                </div>
+
+                            </div>';
+                                    }
+                                }else {
+                                    echo '<div class="col-md-4">
                                 <div class="card-body">
                                     <div class="form-group">
                                         <label>Zbritja(%)</label>
@@ -78,7 +137,10 @@ if (isset($_POST['btnsave'])) {
                                     <button type="submit" class="btn btn-warning" name="btnsave">Regjistro Zbritjen</button>
                                 </div>
 
-                            </div>
+                            </div>';
+                                }
+                            ?>
+                            
                             <div class="col-md-8">
                                 <table id="tbl_category" class="table table-striped table-hover">
                                     <thead>
